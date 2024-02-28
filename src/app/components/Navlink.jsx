36caaee66db786176ea47b8205b4326e79ce8a64
Daniel Maia Rocha <118913634+DanielMaiaRocha@users.noreply.github.com>
2024-02-27 21:06:42 -0300

    Update Navlink.jsx

--- src/app/components/Navlink.jsx
+++ src/app/components/Navlink.jsx
@@ -1,12 +1,10 @@
-"use client"
-import React from "react";
-import Link from "next/link"
+import Link from "next/link";
 
 const Navlink = ({href, title}) => {
     return (
         <Link 
         href={href} 
-        className='block py-2 pl-3 pr-4 font-semibold text-[#121212] sm:text-sl rounded md:p-0 hover:text-[#433d3d]' >
+            className='block py-2 pl-3 pr-4 font-semibold text-[#121212] sm:text-sl rounded md:p-0 hover:text-[#433d3d]' >
           {title}
         </Link>
     )
