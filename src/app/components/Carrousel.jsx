4b7274ea3bc864a2a37cd5438d02e4d043c064c2
hjkshjka <cronobr5@gmail.com>
2024-02-27 23:49:26 -0300

    carrousel commit

--- src/app/components/Carrousel.jsx
+++ src/app/components/Carrousel.jsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react';
 import Slider from 'react-slick'; 
+import Image from 'next/image';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -49,64 +50,64 @@ const Carrousel = () => {
         <div className='relative px-4 md:px-0 block pt-30 pt-md-0'>
             <Slider {...settings} className='p-7 max-w-[1240px] mx-auto'> 
                 <div className="mx-2">
-                <img className="w-[150px] h-[100px] object-contain mx-auto" src="/images/netflix.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/netflix.png" alt="/" width={150} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[80px] h-[100px] object-contain mx-auto" src="/images/amazon.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/amazon.png" alt="/" width={80} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[100px] h-[100px] object-contain mx-auto" src="/images/disney.jpg" alt="" />
+                <Image className="object-contain mx-auto" src="/images/disney.jpg" alt="/" width={100} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[100px] h-[100px] object-contain mx-auto" src="/images/warner.svg" alt="" />
+                <Image className=" object-contain mx-auto" src="/images/warner.svg" alt="/" width={100} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[170px] h-[100px] object-contain mx-auto" src="/images/crunchyroll.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/crunchyroll.png" alt="/" width={170} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[140px] h-[100px] object-contain mx-auto" src="/images/dwa.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/dwa.png" alt="/" width={140} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[80px] h-[100px] object-contain mx-auto" src="/images/deluxe.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/deluxe.png" alt="/" width={80} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[130px] h-[100px] object-contain mx-auto" src="/images/iyunno.jpg" alt="" />
+                <Image className=" object-contain mx-auto" src="/images/iyunno.jpg" alt="/" width={130} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[100px] h-[100px] object-contain mx-auto" src="/images/zap.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/zap.png" alt="/" width={100} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[80px] h-[100px] object-contain mx-auto" src="/images/onegai.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/onegai.png" alt="/" width={80} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[150px] h-[100px] object-contain mx-auto" src="/images/pixelogic.jpg" alt="" />
+                <Image className="object-contain mx-auto" src="/images/pixelogic.jpg" alt="/" width={150} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[130px] h-[100px] object-contain mx-auto" src="/images/audible.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/audible.png" alt="/" width={130} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[200px] h-[120px] object-contain mx-auto" src="/images/baybus.jpg" alt="" />
+                <Image className="object-contain mx-auto" src="/images/baybus.jpg" alt="/" width={210} height={120} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[90px] h-[100px] object-contain mx-auto" src="/images/futura.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/futura.png" alt="/" width={90} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[150px] h-[120px] object-contain mx-auto" src="/images/globolivros.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/globolivros.png" alt="/" width={120} height={100} />
                 </div>
                 <div className="mx-2">
-                <img className="w-[100px] h-[100px] object-contain mx-auto" src="/images/hbo.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/hbo.png" alt="/" width={100} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[180px] h-[100px] object-contain mx-auto" src="/images/lionsgate.svg" alt="" />
+                <Image className="object-contain mx-auto" src="/images/lionsgate.svg" alt="/" width={180} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[80px] h-[100px] object-contain mx-auto" src="/images/mattel.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/mattel.png" alt="/" width={80} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[150px] h-[100px] object-contain mx-auto" src="/images/mgm.png" alt="" />
+                <Image className="object-contain mx-auto" src="/images/mgm.png" alt="/" width={150} height={100}/>
                 </div>
                 <div className="mx-2">
-                <img className="w-[200px] h-[100px] object-contain mx-auto" src="/images/nbcu.svg" alt="" />
+                <Image className="object-contain mx-auto" src="/images/nbcu.svg" alt="/" width={200} height={100}/>
                 </div>
             </Slider>
         </div>
